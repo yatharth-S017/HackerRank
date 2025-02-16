fbe194192e71b170b1cbd9a9f1316c72bc9d4bc3
yatharth <yatharthsingh239@gmail.com>
2025-02-16 12:35:06 +0530

    16F

---
+++ numPowertwo30.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+   int n;
+    scanf("%d", &n);
+    if(n==0){
+        printf("No");
+    }
+    else if( (n&(n-1))==0){
+        printf("Yes");
+    }
+     
+    else{
+        printf("No");
+    }
+    return 0;
+}

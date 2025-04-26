2b467440c97ffb4057952faa4a2bc400b1fafd77
yatharth <yatharthsingh239@gmail.com>
2025-04-26 23:05:27 +0530

    25A

---
+++ Disaruim112.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <math.h>
+
+
+
+int is_disarium_number(int n) {
+    // Implement this function
+    int original = n;
+    int sum = 0;
+    int x = digit(n);
+    int m = x;
+    while(n>0){
+        int rem = n%10;
+        sum += pow(rem,m);
+        m--;
+        n=n/10;
+    }
+    
+    
+    if(original == sum){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+
+int digit(int n){
+    int count =0;
+    while(n>0){
+        n = n/10;
+        count++;
+    }
+    return count;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", is_disarium_number(n));
+    return 0;
+}

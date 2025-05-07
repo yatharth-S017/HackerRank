35d5048011426eb430165d9981146a10f6cc4259
yatharth <yatharthsingh239@gmail.com>
2025-05-07 23:32:06 +0530

    7M

---
+++ countOccurances116.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for(int i=0; i<n; i++){
+        scanf("%d", &arr[i]);
+    }
+    int target;
+    scanf("%d", &target);
+    int count=0;
+    for(int i=0; i<n; i++){
+        if(arr[i] == target){
+            count++;
+        }
+    }
+    
+    printf("%d", count);
+    return 0;
+}

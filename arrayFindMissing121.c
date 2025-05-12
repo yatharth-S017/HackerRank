cad30cf71ac2c71bb61deb87fa8b4bf99ddcd3c7
yatharth <yatharthsingh239@gmail.com>
2025-05-12 23:05:44 +0530

    12M

---
+++ arrayFindMissing121.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+
+    int n;
+    scanf("%d", &n);
+    int arr[n-1];
+    
+    for(int i=0; i<n-1; i++){
+        scanf("%d", &arr[i]);
+    }
+    
+    for(int i=1; i<=n; i++){
+        int flag=0;
+        for(int j=0; j<n-1; j++){
+            if(arr[j] == i){
+                flag++;
+                break;
+            }
+        }
+        
+        if(flag==0){
+            printf("%d", i);
+            break;
+        }
+    }
+    
+    return 0;
+}
